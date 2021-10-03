f6c35860e0010124ae53c4211c9eb620bf97aa4e
Lucas Orsini <lucasorsini@gmail.com>
2021-10-03 18:42:35 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function elegirPlaneta (planeta) {
+    if (planeta == 1) {
+        return planeta = 46;
+    } else {}
+}
+
+
+let pla1 = parseInt(prompt("Elija el planeta: \n 1-Mercurio\n 2-Venus \n 3-Tierra \n 4-Marte \n 5-Jupiter \n 6-Saturno \n 7-Urano \n 8-Neptuno"));
+let pla2 = parseInt(prompt("Elija el planeta: \n 1-Mercurio\n 2-Venus \n 3-Tierra \n 4-Marte \n 5-Jupiter \n 6-Saturno \n 7-Urano \n 8-Neptuno"));
